bf0184bfe926ca0474d22d7f7f08b86c20a6d24a
John Kelly <chippyfork@gmail.com>
2020-12-24 12:05:41 +0000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: {
+    content: ['./src/**/*.njk']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
